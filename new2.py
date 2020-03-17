39f930f25cd7e467de4c4d3a61e625caa18ddbb7
AswanthKT <62300485+AswanthKT@users.noreply.github.com>
2020-03-17 13:09:10 -0700

    Add files via upload

---
+++ new2.py
@@ -0,0 +1,25 @@
+class programs:
+
+    def __init__(self,ai,ml,course,i,desig):
+        self.ai = ai
+        self.ml = ml
+
+print("course 1.) ml or 2.) ai")
+course = input()
+
+print(course)
+
+if (course == 'ml' ):
+ print("mentor or student")
+ desig = input()
+
+ print(desig)
+
+if (desig == 'mentor'):
+    ai = []
+    ai = input("name , time").split()
+
+else:
+
+    print(ai)
+
