b934eb6eed44c5815340b6afdcfd5484a9e88110
AswanthKT <62300485+AswanthKT@users.noreply.github.com>
2020-03-17 11:29:45 -0700

    Add files via upload

---
+++ new.py
@@ -0,0 +1,25 @@
+class programs:
+
+    def __init__(self,ai,ml,course,i):
+        self.ai = ai
+        self.ml = ml
+
+print("course ml or ai")
+course = input()
+
+ 
+ if (course == 10):
+print("mentor or student")
+k = input()
+
+print(course)
+ai = []
+
+ai = input("name , time").split()
+
+
+
+
+
+print(ai)
+
